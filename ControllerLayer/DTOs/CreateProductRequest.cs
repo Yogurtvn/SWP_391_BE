28ba262a48de0261fb4f96b37168b127711bf208
Yogurtvn <128718485+Yogurtvn@users.noreply.github.com>
2026-04-13 23:25:39 +0700

    setup project

--- ControllerLayer/DTOs/CreateProductRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace ControllerLayer.DTOs
-{
-    public class CreateProductRequest
-    {
-        public string Name { get; set; } = string.Empty;
-
-        public decimal Price { get; set; }
-    }
-}
--- ControllerLayer/Program.cs
+++ ControllerLayer/Program.cs
@@ -1,19 +1,11 @@
-using ServiceLayer.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddServiceLayer();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
